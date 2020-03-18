3c62d07c15e5b8a3bc7d006a8e64d915fb79f4b1
kurobanekengo <kurobanekengo@gmail.com>
2020-03-19 00:26:58 +0900

    apiDescriptor修正

--- source/api/apiDescriptor.ts
+++ source/api/apiDescriptor.ts
@@ -1,6 +1,12 @@
-export enum ApiMethod {
+export type ApiMethod = "POST" | "GET" | "DELETE" | "PUT";
+const POST: ApiMethod = "POST";
+const GET: ApiMethod = "GET";
+const DELETE: ApiMethod = "DELETE";
+const PUT: ApiMethod = "PUT";
+
+export const apiMethod = {
   POST, GET, DELETE, PUT
-}
+};
 
 export type ApiBody<REQUEST, RESPONSE> = (req: REQUEST) => RESPONSE;
 
@@ -13,9 +19,19 @@ export class ApiDeclaration<REQ, RES, VERSION> {
     this.method = method;
     this.apiMap = new Map();
   }
-  defineApi(version: VERSION, apiBody: ApiBody<REQ, RES>) {
+  defineApi = (version: VERSION, apiBody: ApiBody<REQ, RES>) => {
     this.apiMap.set(version, apiBody);
-  }
+  };
+  getApi = (version: VERSION) => {
+    const apiPath = `v${version}/${this.resourceName}`;
+    const apiBody = this.apiMap.get(version);
+    if (apiBody == null) {
+      throw new Error(`[error] ${this.method}: ${apiPath} is not implemented`);
+    }
+    return {
+      apiPath, apiBody
+    };
+  };
 }
 
 export const declareApi = <REQ, RES, VERSION>(
