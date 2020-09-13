a62fea77a99416619a7f6cdf5476708c476f51d0
kurobanekengo <kurobanekengo@gmail.com>
2020-09-13 19:33:00 +0900

    Resourceの型定義修正

--- source/model/resources.ts
+++ source/model/resources.ts
@@ -1,7 +1,8 @@
 import {CategoryEntity} from "@entity/categoryEntity";
 import {PasswordEntity} from "@entity/passwordEntity";
+import {Resource} from "@model/resource";
 
 export interface Resources {
- categories: Map<string, CategoryEntity>;
- passwords: Map<string, PasswordEntity>;
+ categories: Resource<CategoryEntity>;
+ passwords: Resource<PasswordEntity>;
 }
