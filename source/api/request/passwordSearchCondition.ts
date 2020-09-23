6f6e0e4799de639baee3f2748be44134232f49d3
kurobanekengo <kurobanekengo@gmail.com>
2020-09-23 20:17:09 +0900

    password検索条件JSON対応

--- source/api/request/passwordSearchCondition.ts
+++ source/api/request/passwordSearchCondition.ts
@@ -1,6 +1,20 @@
 import { CategoryId } from "@entity/categoryEntity";
+import { DefaultJsonTypeConverter } from "dist/converter/jsonTypeConverter/defaultJsonTypeConverter";
+import { isString } from "util";
 
 export interface PasswordSearchCondition {
   keyword: string;
   categoryId: CategoryId
+}
+
+const converter = DefaultJsonTypeConverter
+.getInstance<PasswordSearchCondition>()
+.has('keyword', isString, true)
+.has('categoryId', isString, true)
+;
+
+export class PasswordSearchCondition {
+  static isPasswordSearchCondition(json: unknown): json is PasswordSearchCondition {
+    return converter.canBe(json);
+  }
 }
