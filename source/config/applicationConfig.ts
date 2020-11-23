3c914dd2669176f02e21ffa7349e0119282c8b9f
kurobane.kengo <kurobanekengo@gmail.com>
2020-11-23 16:06:03 +0900

    環境別設定取得処理にTODOコメントを追加

--- source/config/applicationConfig.ts
+++ source/config/applicationConfig.ts
@@ -13,6 +13,7 @@ export interface ApiConfig {
   prefixPath: string;
 }
 
+// TODO nodeの起動パラメータ等から実行環境を判定し対応するコンフィグ情報を返す
 export const getConfig = (): ApplicationConfig => {
   return DevConfig;
 };
